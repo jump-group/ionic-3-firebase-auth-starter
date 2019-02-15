cc85ed08da533eab80709ba5b9eaccfdd6f708f9
JEricaM <erica@jumpcomm.it>
2019-02-15 15:05:53 +0100

    Clean code

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -5,18 +5,15 @@ import { FormGroup} from '@angular/forms';
 @Injectable()
 export class AuthData {
   
-  public signupForm: FormGroup; //Serve per la funzione matching password
+  public signupForm: FormGroup;
   
   constructor() {
-
   }
 
-  // Effettuare il login con email e password (check nel servizio di autenticazione di Firebase)
   loginUser(email: string, password: string): Promise<any> {
     return firebase.auth().signInWithEmailAndPassword(email, password);
   }
 
-  // Inserisce nel database i dati dell'utente registrato
   signupUser(email: string, password: string): Promise<any> {
     return firebase
       .auth()
@@ -32,29 +29,23 @@ export class AuthData {
       });
   }
 
-  // Reset della password
   resetPassword(email: string): Promise<void> {
     return firebase.auth().sendPasswordResetEmail(email);
   }
 
-  // Logout dell'utente
   logoutUser(): Promise<void> {
     return firebase.auth().signOut();
   }
 
-    //Metodo per controllare che le password inserite siano identiche, altrimenti non permette di registrarsi.
-    matchingPasswords(passwordKey: string, confirmPasswordKey: string) {
-      return (group: FormGroup): {[key: string]: any} => {
-        let password = group.controls[passwordKey];
-        let confirmPassword = group.controls[confirmPasswordKey];
-        if (password.value !== confirmPassword.value) {
-          return {
-            mismatchedPasswords: true
-          };
-        }
+  matchingPasswords(passwordKey: string, confirmPasswordKey: string) {
+    return (group: FormGroup): {[key: string]: any} => {
+      let password = group.controls[passwordKey];
+      let confirmPassword = group.controls[confirmPasswordKey];
+      if (password.value !== confirmPassword.value) {
+        return {
+          mismatchedPasswords: true
+        };
       }
     }
-
-
-
+  }
 }
